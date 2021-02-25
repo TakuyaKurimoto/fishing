10681509c6d0a731e46483866660e3dc938a598e
takuyaK <welcom4869@icloud.com>
2021-02-25 16:26:30 +0900

    自動デプロイ

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>釣り掲示板 - @yield('title')@yield('kensaku') </title>
+    <title>釣り掲示板 - @yield('title')</title>
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="https://code.jquery.com/jquery-3.5.1.js"
